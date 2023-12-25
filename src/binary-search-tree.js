80c923ad938ff581a921105cb03f0c1d7c361e06
victoria <v.suprun1961@gmail.com>
2023-12-26 00:09:24 +0200

    feat: complete task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -7,69 +7,7 @@
 * using Node from extensions
 // */
 
-
-
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
-}
-
 class BinarySearchTree {
-  constructor() {
-    this.root = null;
-   }
-
-   add(value) {
-    const newNode = new Node(value)
-
-    if (!this.root) {
-      this.root = newNode;
-      return;
-   }
-
-   let currentNode = this.root;
-
-   while(currentNode) {
-    if (newNode.value < currentNode.value) {
-      if (!currentNode.left) {
-        currentNode.left = newNode;
-        return;
-      }
-      currentNode = currentNode.left;
-   } else {
-      if (!currentNode.right) {
-        currentNode.right = newNode;
-        return;
-      }
-      currentNode = currentNode.right;
-  }
-
-  }
-    }
-    traverseDFS() {
-
-    }
-    traverseBFS() {
-      
-    }
-}
-const myTree = new BinarySearchTree();
-      myTree.add(8);
-      myTree.add(7);
-      myTree.add(9);
-      myTree.add(5);
-      myTree.add(10);
-      myTree.add(20);
-      myTree.add(6);
-      myTree.add(2);
-      myTree.add(11);
-console.log(myTree);
-
-
-
   // root() {
   //   throw new NotImplementedError('Not implemented');
   //   // remove line with error and write your code here
@@ -108,3 +46,4 @@ console.log(myTree);
 //  module.exports = {
 //   BinarySearchTree
 // };
+};
