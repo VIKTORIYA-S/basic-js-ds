6ec04c9581f2bd2375bf408510b66f7280deb57f
victoria <v.suprun1961@gmail.com>
2025-01-04 18:42:50 +0200

    feat: complete task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -3,47 +3,56 @@
 // const { Node } = require('../extensions/list-tree.js');
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
+*Реализовать простое двоичное дерево поиска согласно описанию задачи
+* с использованием Node из расширений
 // */
 
 class BinarySearchTree {
-  // root() {
-  //   throw new NotImplementedError('Not implemented');
-  //   // remove line with error and write your code here
-  // }
+  root() {
+    return this.rootNode;
+    // remove line with error and write your code here
+  }
 
-  // add(/* data */) {
-  //   throw new NotImplementedError('Not implemented');
-  //   // remove line with error and write your code here
-  // }
+  add(data) {
+    const newNode = new Node(data);
+    if (this.rootNode === null) {
+      this.rootNode = newNode;
+    } else {
+      this.insertNode(this.rootNode, newNode);
+    }
+    // remove line with error and write your code here
+  }
 
-  // has(/* data */) {
-  //   throw new NotImplementedError('Not implemented');
-  //   // remove line with error and write your code here
-  // }
+  has(data) {
+    return this.search(this.rootNode, data) !== null;throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+  }
 
-  // find(/* data */) {
-  //   throw new NotImplementedError('Not implemented');
-  //   // remove line with error and write your code here
-  // }
+  find(data) {
+    return this.search(this.rootNode, data);
+    // remove line with error and write your code here
+  }
 
-  // remove(/* data */) {
-  //   throw new NotImplementedError('Not implemented');
-  //   // remove line with error and write your code here
-  // }
+  remove(data) {
+    this.rootNode = this.removeNode(this.rootNode, data);
+    // remove line with error and write your code here
+  }
 
-  // min() {
-  //   throw new NotImplementedError('Not implemented');
-  //   // remove line with error and write your code here
-  // }
+  min() {
+    const node = this.findMinNode(this.rootNode); return node ? node.data : null;
+    // remove line with error and write your code here
+  }
 
-  // max() {
-  //   throw new NotImplementedError('Not implemented');
-  //   // remove line with error and write your code here
-  // }
+  max() {
+    let node = this.rootNode;
+    while (node && node.right !== null) {
+      node = node.right;
+    }
+    return node ? node.data : null;
+  }
+    // remove line with error and write your code here
+  }
 
 //  module.exports = {
 //   BinarySearchTree
 // };
-};
--- src/stack.js
+++ src/stack.js
@@ -2,6 +2,7 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Implement the Stack with a given interface via array.
+ * Реализовать стек с заданным интерфейсом через массив.
  *
  * @example
  * const stack = new Stack();
@@ -10,6 +11,11 @@ const { NotImplementedError } = require('../extensions/index.js');
  * stack.peek(); // returns the peek, but doesn't delete it, returns 1
  * stack.pop(); // returns the top element from stack and deletes it, returns 1
  * stack.pop(); // undefined
+ * 
+ * stack.push(1); // добавляет элемент в стек
+* stack.peek(); // возвращает peek, но не удаляет его, возвращает 1
+* stack.pop(); // возвращает верхний элемент из стека и удаляет его, возвращает 1
+* stack.pop(); // не определено
  *
  */
 class Stack {
